3c7a07dacf7a651aa2e73b4fa71499c813eb7145
HossainRafi <hossainrafi71@gmail.com>
2022-12-06 16:41:46 +0600

    get all countries

--- src/components/AllCountries/AllCountries.jsx
+++ src/components/AllCountries/AllCountries.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { apiURL } from "../../util/api";
 
 const AllCountries = () => {
-  return (
-    <div>AllCountries</div>
-  )
-}
+  const [countries, setCountries] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState("");
 
-export default AllCountries
+  const getAllCountries = async () => {
+    try {
+      const res = await fetch(`${apiURL}/all`);
+
+      if (!res.ok) throw new Error("Something went wrong!");
+
+      const data = await res.json();
+
+      console.log(data);
+
+      setCountries(data);
+
+      setIsLoading(false);
+    } catch (error) {
+      setIsLoading(false);
+      setError(error.message);
+    }
+  };
+
+  useEffect(() => {
+    getAllCountries();
+  }, []);
+
+  return <div className="all__country__wrapper"></div>;
+};
+
+export default AllCountries;
