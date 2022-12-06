77354d86600834a5e96d419a353acfeb34be3660
HossainRafi <hossainrafi71@gmail.com>
2022-12-06 18:08:48 +0600

    send data to searchinput

--- src/components/AllCountries/AllCountries.jsx
+++ src/components/AllCountries/AllCountries.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { apiURL } from "../../util/api";
 import { Link } from "react-router-dom";
+import SearchInput from "../Search/SearchInput";
 
 const AllCountries = () => {
   const [countries, setCountries] = useState([]);
@@ -26,12 +27,33 @@ const AllCountries = () => {
     }
   };
 
+  const getCountryByName = async (countryName) => {
+    try {
+      const res = await fetch(`${apiURL}/name/${countryName}`);
+
+      if (!res.ok) throw new Error("Not found any country!");
+
+      const data = await res.json();
+      setCountries(data);
+
+      setIsLoading(false);
+    } catch (error) {
+      setIsLoading(false);
+      setError(error.message);
+    }
+  };
+
   useEffect(() => {
     getAllCountries();
   }, []);
 
   return (
     <div className="all__country__wrapper">
+      <div className="country__top">
+        <div className="search">
+          <SearchInput onSearch={getCountryByName} />
+        </div>
+      </div>
       <div className="country__bottom">
         {isLoading && !error && <h4>Loading........</h4>}
         {error && !isLoading && <h4>{error}</h4>}
