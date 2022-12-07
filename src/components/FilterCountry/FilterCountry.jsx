54036e5d69be06309a0672750d9aabdd9134484c
HossainRafi <hossainrafi71@gmail.com>
2022-12-07 15:29:53 +0600

    filter done

--- src/components/FilterCountry/FilterCountry.jsx
+++ src/components/FilterCountry/FilterCountry.jsx
@@ -9,14 +9,24 @@ const FilterCountry = ({ onSelect }) => {
   return (
     <select
       onChange={selectHandler}
-      className="px-2 lg:px-4 py-1 lg:py-2 text-lg lg:text-xl font-normal lg:font-medium outline-none bg-gray-400"
+      className="px-2 lg:px-4 py-1 lg:py-2 text-lg lg:text-xl font-normal lg:font-medium outline-none bg-gray-400 rounded-[5px] text-slate-600"
     >
-      <option>Filter By Region</option>
-      <option value="Asia">Asia</option>
-      <option value="Europe">Europe</option>
-      <option value="America">America</option>
-      <option value="Africa">Africa</option>
-      <option value="Oceania">Oceania</option>
+      <option className="text-black">Filter By Region</option>
+      <option value="Asia" className="text-black">
+        Asia
+      </option>
+      <option value="Europe" className="text-black">
+        Europe
+      </option>
+      <option value="America" className="text-black">
+        America
+      </option>
+      <option value="Africa" className="text-black">
+        Africa
+      </option>
+      <option value="Oceania" className="text-black">
+        Oceania
+      </option>
     </select>
   );
 };
--- src/components/Search/SearchInput.jsx
+++ src/components/Search/SearchInput.jsx
@@ -12,7 +12,7 @@ const SearchInput = ({ onSearch }) => {
   return (
     <form onSubmit={submitHandler}>
       <input
-        className="px-2 lg:px-4 py-1 lg:py-2 text-lg lg:text-xl font-normal lg:font-medium outline-none bg-gray-400 placeholder-slate-600"
+        className="px-2 lg:px-4 py-1 lg:py-2 text-lg lg:text-xl font-normal lg:font-medium outline-none bg-gray-400 placeholder-slate-600 rounded-[5px]"
         type="text"
         placeholder="Enter Country Name"
         value={input}
